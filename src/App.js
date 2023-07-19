1ebb76f74cb9a7b826931fd8c527a7215023f5f0
Darios Petrov <darios.petrov93@gmail.com>
2023-07-19 11:49:12 +0300

    adding css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './style.css'
 function App() {
   return (
     <div className="calculator-grid">
-      <div className="output"> </div>
-      <div className="previous-openrand"> </div>
-
-      <div className="current-openrand"> </div>
-
+      <div className="output">
+        <div className="previous-openrand"></div>
+        <div className="current-openrand"></div>
+      </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
       <button>÷</button>
@@ -22,18 +21,10 @@ function App() {
       <button>7</button>
       <button>8</button>
       <button>9</button>
+      <button>-</button>
       <button>.</button>
       <button>0</button>
       <button className="span-two">=</button>
-
-
-
-      
-
-
-
-      
-  
     </div>
   );
 }
